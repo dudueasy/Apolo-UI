03c22033d7359fe1cd0e10f1ca98e627bd8fdfb3
dudueasy <dudueasy@126.com>
2019-05-21 08:47:33 +0800

    添加了 <Icon> 组件和一些 Icon 的图案

--- lib/icon/icon.tsx
+++ lib/icon/icon.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
+// 引入 icons 目录下的所有 svg
+import  '../../utils/importAllSvg';
+
+
 interface Props {
-   name: string
+    name: string
 }
 
 const Icon: React.FC<Props> = (props) => {
-    return <div>{props.name}</div>;
+    return (
+        <div>
+            <svg>
+                <use xlinkHref={`#${props.name}`}/>
+            </svg>
+        </div>
+    );
 };
+
 
 export default Icon;
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from './button';
+import Icon from './icon/icon';
+import ScrollExample from './scroll/scroll.example';
 
 // hello
 if (module.hot) {
     module.hot.accept();
 }
 
-ReactDOM.render(<Button/>, document.querySelector('#app'));
-
-
-
-
-
+ReactDOM.render(
+    <div>
+        <Icon name='qq'/>
+        <Icon name='alipay'/>
+        <Icon name='wechat'/>
+    </div>
+    , document.querySelector('#app'));
