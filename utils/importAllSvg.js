39228f0d74f09704c00ad8d448c5e470d4e0d0f7
dudueasy <dudueasy@126.com>
2019-05-22 21:50:08 +0800

    添加了一个 importAll 脚本用来引入icons 所依赖的所有的 svg 文件

---
+++ utils/importAllSvg.js
@@ -0,0 +1,28 @@
+// let importAll = (requireContext:any) => requireContext.keys().forEach(requireContext);
+//
+// try {
+//   console.log(1111111111111111)
+//   importAll(require.context('../icon_svg/', true, /\.svg$/));
+// } catch (e) {
+//   console.log(e)
+// }
+
+
+let cache = {};
+
+// 需要注意的是这个函数由于引用了 webpack api, 需要在编译时调用, 因此并不能接收参数
+function importAllSvg() {
+
+  const fn = (requireContext) => requireContext.keys()
+    .forEach(id => {
+      cache[id] = requireContext(id);
+    });
+
+
+  fn(require.context('../icon_svg/', false, /\.svg$/));
+  // fn(require.context(path, false, filenameRegex));
+}
+
+importAllSvg();
+
+
