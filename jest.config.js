ad6c8b320862c201a73b715f48915d9636d450b7
dudueasy <dudueasy@126.com>
2019-05-24 01:03:04 +0800

    update jest & typescript configuration to allow typescript compile js for unit testing

--- jest.config.js
+++ jest.config.js
@@ -7,13 +7,14 @@ module.exports = {
   ],
   preset: 'ts-jest'
   ,
-  // transform: {
-  //   // '^.+\\.jsx?$': 'babel-jest',  // 启用此项需要安装 babel 依赖.
-  //   '^.+\\.(t|j)sx?$': 'ts-jest',
-  // },
+  transform: {
+    // '^.+\\.jsx?$': 'babel-jest',  // 启用此项需要安装 babel 依赖.
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
   setupFilesAfterEnv: ['jest-enzyme'],   // 添加 jest 测试工具模块
   testEnvironment: 'enzyme',            // 定义测试时的代码环境
   testEnvironmentOptions: {
     enzymeAdapter: 'react16',            // 定义 enzyme 环境版本为 react16 (默认值)
   },
+  verbose:true
 };
