4cbfb0c769b4c5c6a932c130e33a157934c0bbe8
dudueasy <dudueasy@126.com>
2019-06-22 03:41:50 +0800

    更新 circleci 配置
    x
    
    0.3.1
    
    x
    
    0.3.2
    
    x
    
    0.3.3
    
    x
    
    0.3.4
    
    x
    
    0.3.5
    
    x
    
    0.3.6
    
    0.3.7
    
    x
    
    0.3.8
    
    x
    
    0.3.9
    
    x
    
    0.3.10
    
    x
    
    0.3.11
    
    x
    
    0.3.12
    
    x
    
    0.3.13
    
    x
    
    0.3.14
    
    x
    
    0.3.15
    
    x
    
    0.3.16
    
    x
    
    0.3.17
    
    x
    
    0.3.18
    
    x
    
    x
    
    0.3.19
    
    x
    
    0.3.20

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,12 @@
 
 if [ $1 ]
 then
-    npm version ${1} && git push
+    npm version ${1} \
+    && git checkout deploy  \
+    && git merge master \
+    && git push --all \
+    && git checkout master
+
 else
     echo "请输入要更新的版本号"
 fi
