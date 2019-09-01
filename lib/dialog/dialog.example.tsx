7768bdfe0de87e4f6c30349b5bed3e9d942f1bde
dudueasy <dudueasy@126.com>
2019-09-01 21:45:31 +0800

    feat: 添加 Dialog 组件的错误表现  Dialog 的遮罩层无法遮住其他组件

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,27 +1,55 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
 import Dialog from './dialog';
 
 
-export default function(){
+export default function () {
 
-  const [visible, toggleVisible] = useState<boolean>(false)
+  const [visible, toggleVisible] = useState<boolean>(false);
+
+  const handleOpen = () => { toggleVisible(true); };
+  const handleClose = () => { toggleVisible(false);};
+
+  const [Xvisible, toggleXVisible] = useState<boolean>(false);
 
   return (
     <div>
-      <button onClick={()=>{  toggleVisible((visible)=>(!visible))}}>
-        toggleDialog
-      </button>
-      <Dialog
-        visible={visible}
-        buttons={[
-          <button onClick={()=>{toggleVisible(false)}}>
-            cancel
-          </button> ,
-          <button onClick={()=>{toggleVisible(false)}}>
-            ok
-          </button>
-        ]}
-      >11111</Dialog>
+      <div style={{position:"relative", zIndex:10}}>
+        <h2>Example1</h2>
+        <button onClick={() => {toggleVisible((visible) => (!visible));}}>
+          toggleDialog
+        </button>
+        <Dialog
+          visible={visible}
+          onClose={handleClose}
+          buttons={[
+            <button onClick={()=>{toggleVisible(false)}}>
+              cancel
+            </button>,
+            <button onClick={()=>{toggleVisible(false)}}>
+              ok
+            </button>
+          ]}
+        >11111</Dialog>
+      </div>
+
+      <div>
+        <h2>Example2</h2>
+        <button onClick={() => {toggleXVisible((visible) => (!visible));}}>
+          toggleDialog
+        </button>
+        <Dialog
+          visible={Xvisible}
+          onClose={()=>{ toggleXVisible(false)}}
+          buttons={[
+            <button onClick={()=>{toggleXVisible(false)}}>
+              cancel
+            </button>,
+            <button onClick={()=>{toggleXVisible(false)}}>
+              ok
+            </button>
+          ]}
+        >11111</Dialog>
+      </div>
     </div>
   );
 }
