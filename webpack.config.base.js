ecd54c3ff3b7a0e3a999a6b950ee06e305032ed5
dudueasy <dudueasy@126.com>
2019-11-10 01:23:48 +0800

    feat: 引入 file-loader

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -25,6 +25,7 @@ module.exports = {
         test: /\.s?[ac]ss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {test: /\.(png|jpg|gif)$/, loader: 'file-loader'}
     ],
   },
 }
