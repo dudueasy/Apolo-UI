5b373451b458dab3a3efc4605d07d8113ac75fd1
dudueasy <dudueasy@126.com>
2019-11-10 12:41:06 +0800

    refactor: remove comment

--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -35,4 +35,3 @@ export {default as Content} from './content';
 export {default as Footer} from './footer';
 export {default as Header} from './header';
 export {default as Sider} from './sider';
-// export { default as Content } from './content'
