c75fcc723f0cb31bdc4e022d2a81ab2a0110987b
dudueasy <dudueasy@126.com>
2020-08-07 00:10:55 +0800

    feat:  计算出滚动条的 高度 和 滚动距离

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,4 +1,4 @@
-import React, {HTMLAttributes} from 'react';
+import React, {HTMLAttributes, UIEventHandler, useLayoutEffect, useRef, useState} from 'react';
 import combineClassNames, {scopedClassMaker} from '../utils/className';
 import './style.scss';
 
@@ -7,22 +7,52 @@ interface ScrollProps extends HTMLAttributes<HTMLDivElement> {};
 const sc = scopedClassMaker('apoloUI-scroll');
 
 const Scroll: React.FC<ScrollProps> = (props) => {
-
-  // const width = scrollbarWidth();  // 该函数在 mac 上表现异常, 所以不使用
-  const width = '-16px';
+  // const offsetDistance = scrollbarWidth();  // 该函数在 mac 上表现异常, 所以不使用
   const {children, className, ...rest} = props;
-  const onScroll = () => {
 
+  const offsetDistance = -16;
+  const barWidth = 16;
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const [barHeight, setBarHeight] = useState(0);
+  const [barTop, setBarTop] = useState(0);
+
+  useLayoutEffect(() => {
+    const scrollHeight = containerRef!.current!.scrollHeight;
+    const viewHeight = containerRef!.current!.clientHeight;
+    setBarHeight(viewHeight * viewHeight / scrollHeight);
+  }, []);
+
+  // 得到每次滚动的距离
+  const onScroll: UIEventHandler<HTMLDivElement> = (e) => {
+    let current = containerRef.current!;
+    const scrollHeight = current.scrollHeight;
+    const viewHeight = current.clientHeight;
+    const scrollTop = current.scrollTop;
+
+    setBarTop(barHeight * scrollTop / viewHeight);
   };
+
   return (
     <div
       className={combineClassNames(sc(), className)}
       {...rest}
     >
-      <div className={sc('inner')} style={{'right': width}}
-           onScroll={onScroll}
+      <div
+        className={sc('inner')}
+        // style={{right: offsetDistance, bottom: offsetDistance}}
+        style={{right: offsetDistance}}
+        onScroll={onScroll}
+        ref={containerRef}
       >
         {children}
+      </div>
+      <div className={sc('track')} style={{width: barWidth}}>
+        <div className={sc('scrollBar')} style={{
+          width: barWidth,
+          height: barHeight,
+          transform: `translateY(${barTop}px)`
+        }}/>
       </div>
     </div>
   );
